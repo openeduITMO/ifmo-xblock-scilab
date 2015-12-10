654c2c84d082538811c75bb255c711be108049de
Dmitry Ivanyushin <defance@gmail.com>
2015-12-10 15:22:35 +0300

    Спавн scilab'а вынесен отдельно

--- xblock_scilab/tasks.py
+++ xblock_scilab/tasks.py
@@ -9,16 +9,76 @@ import json
 import time
 import zipfile
 import os
+import pwd
+import logging
+from path import path
 
+logger = logging.getLogger(__name__)
 
 SCILAB_EXEC = "/ifmo/app/scilab-5.5.2/bin/scilab-adv-cli"
-SCILAB_STUDENT_CMD = "exec('%s/solution.sce'); exit;"
-SCILAB_INSTRUCTOR_CMD = "exec('%s/checker.sce'); exit;"
+SCILAB_STUDENT_CMD = "%s/solution.sce"
+SCILAB_INSTRUCTOR_CMD = "%s/checker.sce"
+SCILAB_EXEC_SCRIPT = "exec('%s'); exit;"
 
 
 class ScilabSubmissionGrade(GraderTaskBase):
 
+    @staticmethod
+    def _demote(user_uid=os.geteuid(), user_gid=os.getegid()):
+        def result():
+            os.seteuid(user_uid)
+            os.setegid(user_gid)
+        return result
+
+    def _spawn_scilab(self, filename, cwd=None, timeout=None, extra_env=None):
+        """
+        Запускает инстанс scilab'а с указанным файлом и параметрами. Возвращает
+        результат выполнения.
+
+        :param filename: Имя файла для исполнения
+        :param cwd: Рабочая директория, по умолчанию является директориией, где
+                    располагается скрипт для исполнения
+        :param timeout: Время на исполнение в секундах
+        :param extra_env: Дополнительные переменные окружения
+        :return: Результат выполнения
+        """
+
+        # Устанавливаем рабочую директорию, если необходимо
+        if cwd is None:
+            cwd = path(filename).dirname()
+            logger.warning("No cwd specified for scilab_spawn, "
+                           "default is used: %s", cwd)
+
+        # Устанавливаем окружение
+        env = os.environ.copy()
+        if isinstance(extra_env, dict):
+            env.update(extra_env)
+
+        # Запускаем процесс
+        process = Popen([SCILAB_EXEC, '-e', SCILAB_EXEC_SCRIPT % filename],
+                        cwd=cwd, env=env,
+                        preexec_fn=ScilabSubmissionGrade._demote())
+
+        # Убиваем по таймауту или ждём окончания исполнения, если он не задан
+        if timeout is None:
+            process.wait()
+        else:
+            time.sleep(timeout)
+            process.kill()
+
+        # Возвращаем результат исполнения
+        return {
+            'code': process.returncode,
+            # 'stdout': process.stdout.read(),
+        }
+
     def grade(self, student_input, grader_payload):
+        """
+
+        :param student_input:
+        :param grader_payload:
+        :return:
+        """
 
         default_grade = {
             'msg': 'UNKNOWN ERROR',
@@ -42,11 +102,8 @@ class ScilabSubmissionGrade(GraderTaskBase):
         student_archive.extractall(full_path)
 
         # Процессу разрешено выполняться только 2 секунды
-        process = Popen([SCILAB_EXEC, '-e', SCILAB_STUDENT_CMD % full_path], cwd=full_path, env=os.environ.copy())
-        print full_path
-        time.sleep(2)
-        process.kill()
-
+        filename = SCILAB_STUDENT_CMD % full_path
+        student_code = self._spawn_scilab(filename, timeout=2)
 
         instructor_filename = grader_payload.get('filename')
 
@@ -58,9 +115,8 @@ class ScilabSubmissionGrade(GraderTaskBase):
             default_grade['msg'] = 'INSTRUCTOR UNPACK ERROR'
             return default_grade
 
-        process = Popen([SCILAB_EXEC, '-e', SCILAB_INSTRUCTOR_CMD % full_path], cwd=full_path, env=os.environ.copy())
-        time.sleep(2)
-        process.kill()
+        filename = SCILAB_INSTRUCTOR_CMD % full_path
+        checker_code = self._spawn_scilab(filename, timeout=2)
 
         try:
             f = open(full_path + '/checker_output')
