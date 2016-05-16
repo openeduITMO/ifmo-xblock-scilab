1f6267616ad9c1caf784e619d8241c82978e8b1f
Dmitry Ivanyushin <defance@gmail.com>
2016-05-16 19:31:43 +0300

    Поднята версия

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='ifmo-xblock-scilab',
-    version='4.0',
+    version='4.1',
     install_requires=[
         'django',
         'path.py',
