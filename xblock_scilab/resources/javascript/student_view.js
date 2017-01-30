9674477319aac33a9f54e34360fbfc740f845068
Dmitry Ivanyushin <defance@gmail.com>
2017-01-30 16:20:19 +0300

    Обобщена js-вь.ха хблока

--- xblock_scilab/resources/javascript/student_view.js
+++ xblock_scilab/resources/javascript/student_view.js
@@ -1,25 +1,29 @@
 function ScilabXBlockStudentView(runtime, element)
 {
-    var urls = {
+    var self = this;
+
+    ScilabXBlockStudentView.superclass.constructor.apply(this, runtime, element);
+
+    self.urls = {
         upload_logic: runtime.handlerUrl(element, 'upload_submission'),
         get_user_data: runtime.handlerUrl(element, 'get_user_data'),
         download_archive: runtime.handlerUrl(element, 'download_archive')
     };
 
-    var upload_logic = {
-        url: urls.upload_logic,
+    self.upload_logic = {
+        url: self.urls.upload_logic,
         add: function (e, data) {
             var xblock = $(element).find('.ifmo-xblock-student');
-            xblock.find('.upload_container').html(template.upload_selected({
+            xblock.find('.upload_container').html(self.template.upload_selected({
                 'filename': data.files[0].name
             }));
             xblock.find(".upload_another").on('click', function () {
-                xblock.find('.upload_container').html(template.upload_input());
-                xblock.find(".file_upload").fileupload(upload_logic);
+                xblock.find('.upload_container').html(self.template.upload_input());
+                xblock.find(".file_upload").fileupload(self.upload_logic);
             });
             xblock.find(".upload_do").on('click', function () {
                 xblock.find(".upload_do").text("Uploading...");
-                helpers.disable_controllers(element);
+                self.helpers.disable_controllers(element);
                 data.submit();
             });
         },
@@ -29,7 +33,7 @@ function ScilabXBlockStudentView(runtime, element)
             xblock.find(".upload_do").text("Uploading... " + percent + "%");
         },
         start: function() {
-            helpers.disable_controllers(element);
+            this.helpers.disable_controllers(element);
         },
         done: function (e, data) {
             render(data.result);
@@ -40,52 +44,13 @@ function ScilabXBlockStudentView(runtime, element)
             alert('При загрузке архива с решением произошла ошибка');
         },
         always: function() {
-            helpers.enable_controllers(element);
+            this.helpers.enable_controllers(element);
         }
     };
 
-    var get_template = function(tmpl){
-        return _.template($(element).find(tmpl).text());
-    };
-
-    var template = {
-        main: get_template('script.ifmo-xblock-template-base'),
-        upload_input: get_template("script.scilab-template-upload-input"),
-        upload_selected: get_template("script.scilab-template-upload-selected"),
-        annotation: get_template("script.scilab-template-annotation")
-    };
-
-    var helpers = {
-
-        deplainify: function(obj)
-        {
-            for (var key in obj) {
-                try {
-                    if (obj.hasOwnProperty(key)) {
-                        obj[key] = deplainify(JSON.parse(obj[key]));
-                    }
-                } catch (e) {
-                    console.log('failed to deplainify', obj);
-                }
-            }
-            return obj;
-        },
-
-        disable_controllers: function(context)
-        {
-            $(context).find("input,button").addClass('disabled').attr("disabled", "disabled");
-        },
-
-        enable_controllers: function(context)
-        {
-            $(context).find("input,button").removeClass('disabled').removeAttr("disabled");
-        }
-
-    };
-
-    var hooks = {
+    self.add_hooks(self, {
         render_student_answer: function(data) {
-            console.log(data);
+            console.log('render_student_answer()');
 
             // Поскольку у нас нет доступа к идентификатору решения здесь,
             // нам нужен дополнитеьный хендлер, получающий файл по его SHA,
@@ -93,8 +58,8 @@ function ScilabXBlockStudentView(runtime, element)
             var student_file_id = data.sha1;
             var instructor_file_id = /([^\/]*)$/.exec(data.instructor_real_path)[0];
 
-            var student_url = urls.download_archive + '/student?' + student_file_id;
-            var instructor_url = urls.download_archive + '/instructor_prev?' + instructor_file_id;
+            var student_url = self.urls.download_archive + '/student?' + student_file_id;
+            var instructor_url = self.urls.download_archive + '/instructor_prev?' + instructor_file_id;
 
             return '<p>' +
                 '<a href="' + student_url + '" class="button">Скачать решение ' + data.filename + '</a> ' +
@@ -102,38 +67,37 @@ function ScilabXBlockStudentView(runtime, element)
                 '</p>';
         },
         render_annotation: function(data) {
-            return template.annotation(data);
+            return self.template.annotation(data);
         }
-    };
+    });
 
-    /*================================================================================================================*/
-
-    function pregenerated_replacer(str, pregen_arr) {
-        var replacer = function(pregen_arr) {
-            var arr = pregen_arr.slice();
-            return function () {
-                return arr.shift();
+    self.add_helpers(self, {
+        pregenerated_replacer: function (str, pregen_arr) {
+            var replacer = function (pregen_arr) {
+                var arr = pregen_arr.slice();
+                return function () {
+                    return arr.shift();
+                }
+            };
+            if (pregen_arr != null) {
+                return str.replace(/%s/g, replacer(pregen_arr));
+            } else {
+                // В превью студии нет прегена, потому что там нет пользователя как такового
+                return str;
             }
-        };
-        if (pregen_arr != null) {
-            return str.replace(/%s/g, replacer(pregen_arr));
-        } else {
-            // В превью студии нет прегена, потому что там нет пользователя как такового
-            return str;
         }
+    });
 
-    }
-
-    function render(data)
+    self.render = function(data)
     {
         var xblock = $(element).find('.ifmo-xblock-student');
         var template_content = data.task_status != 'GENERATING' ?
-            pregenerated_replacer(template.main(data), data.pregenerated) : template.main(data);
+            self.helpers.pregenerated_replacer(self.template.main(data), data.pregenerated) : self.template.main(data);
         xblock.find('.ifmo-xblock-content').html(template_content);
 
         if (data.allow_submissions) {
-            xblock.find('.upload_container').html(template.upload_input());
-            xblock.find(".file_upload").fileupload(upload_logic);
+            xblock.find('.upload_container').html(self.template.upload_input());
+            xblock.find(".file_upload").fileupload(self.upload_logic);
         }
 
         if (data.task_status == 'QUEUED') {
@@ -142,21 +106,23 @@ function ScilabXBlockStudentView(runtime, element)
 
         if (data.task_status != 'IDLE') {
             setTimeout(function(){
-                $.post(urls.get_user_data, '{}', function(data) {
-                    render(data);
+                $.post(self.urls.get_user_data, '{}', function(data) {
+                    self.render(data);
                 }).fail(function(){
                     console.log('error');
                 })
             }, 5000);
         }
 
-        $(element).find('a.staff-download-instructor-archive').attr('href', urls.download_archive + '/instructor');
+        $(element).find('a.staff-download-instructor-archive').attr('href', self.urls.download_archive + '/instructor');
 
         MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-    }
+    };
 
-    function init_xblock($, _)
+    self.init_xblock = function($, _)
     {
+        ScilabXBlockStudentView.superclass.init_xblock.call(self, $, _);
+
         var xblock = $(element).find('.ifmo-xblock-base');
         var context = xblock.data('context');
 
@@ -165,21 +131,21 @@ function ScilabXBlockStudentView(runtime, element)
             $(element).find('.instructor-info-action').leanModal();
         }
 
-        render(context);
-    }
+        var template = _.partial(self.get_template, element);
+        self.add_templates(self, {
+            main: template('script.ifmo-xblock-template-base'),
+            upload_input: template("script.scilab-template-upload-input"),
+            upload_selected: template("script.scilab-template-upload-selected"),
+            annotation: template("script.scilab-template-annotation")
+        });
+
+        self.render(context);
+    };
 
     $(function(){
-        if (require === undefined) {
-            function loadjs(url) {
-                $("<script>").attr("type", "text/javascript").attr("src", url).appendTo(element);
-            }
-            loadjs("/static/js/vendor/jQuery-File-Upload/js/jquery.iframe-transport.js");
-            loadjs("/static/js/vendor/jQuery-File-Upload/js/jquery.fileupload.js");
-            init_xblock($, _);
-        } else {
-            require(["jquery", "underscore", "jquery.fileupload"], init_xblock);
-        }
-        init_modals(runtime, element, $, _, hooks, helpers);
+        self.pre_init_xblock(self, runtime, element, $, _);
     });
 
 }
+
+xblock_extend(ScilabXBlockStudentView, IfmoXBlockStudentView);
